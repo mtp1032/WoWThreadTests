03872640c1ace1f44b2f544a5e02945f254fe072
Michael Peterson <mtpeterson1948@gmail.com>
2022-11-05 16:41:46 -0600

    Update ThreadStats.lua
    Added functionality to support sorting threads by the name of the addon in which they were created.

--- ThreadStats.lua
+++ ThreadStats.lua
@@ -19,45 +19,47 @@ local SIG_WAKEUP        = dispatch.SIG_WAKEUP  -- You've returned prematurely fr
 local SIG_RETURN        = dispatch.SIG_RETURN  -- cleanup state and return from the action routine
 local SIG_NONE_PENDING  = dispatch.SIG_NONE_PENDING    -- default value. Means no signal is pending
 
-
--- entry = { threadId, ticksPerYield, actualTimeSuspended, numYields, lifeTime }
+-- entry = {addonName, threadId, ticksPerYield, yieldCount, measuredTimeSuspended, lifeTime }
 function stats:printEntry( e )
     local result = {SUCCESS, EMPTY_STR, EMPTY_STR}
 
-	local threadId 					= e[1]
-	local ticksPerYield				= e[2]
-	local actualTimeSuspended 		= e[3]				-- milliseconds
-	local numYields					= e[4]
-	local actualLifeTime			= e[5]			-- milliseconds
-	local calculatedTimeSuspended 	= e[2] * (1/GetFramerate()) * 1000 * numYields 	--milliseconds
-	local efficiencyPerCent			= ((calculatedTimeSuspended) / actualTimeSuspended) * 100
+	local addonName					= e[1]
+	local threadId 					= e[2]
+	local ticksPerYield				= e[3]
+	local yieldCount				= e[4]
+	local measuredTimeSuspended 	= e[5]			-- milliseconds
+	local measuredLifetime			= e[6]			-- milliseconds
+	-- local calculatedTimeSuspended 	= ticksPerYield * yieldCount * framerate
 
-	local s1 = sprintf("\nThread Id: %d\n", threadId )
-	local s2 = sprintf("  Millseconds per tick: %.3f\n", (1/GetFramerate()) * 1000 )
-	local s3 = sprintf("  Ticks per Yield: %d, thread interval time %.2f ms\n", ticksPerYield, ticksPerYield * (1/GetFramerate()) * 1000 )
-	local s4 = sprintf("  Yield count: %d\n", numYields )
-	local s5 = sprintf("  Calculated time suspended: %.2f ms\n", calculatedTimeSuspended )
-	local s6 = sprintf("  Actual time suspended: %.2f ms\n", actualTimeSuspended)
-	local s7 = sprintf("  Efficiency: %.2f%%\n", efficiencyPerCent )
-	local s8 = sprintf("  Lifetime: %d ms.\n\n", actualLifeTime )
+	local meanFramerate = measuredTimeSuspended/(ticksPerYield * yieldCount )
+	local totalTicks = measuredTimeSuspended / meanFramerate
+	congestion 	= 1 - (measuredTimeSuspended / measuredLifetime)
 
-	mf:postMsg( s1 .. s2 .. s3 .. s4 .. s5 .. s6 .. s7 .. s8 )
+	-- local s1 = sprintf("\nThread Id: %d\n", threadId )
+	-- local s2 = sprintf("  Milliseconds per tick: %.3f\n", (1/GetFramerate()) * 1000 )
+	-- local s3 = sprintf("  Ticks per Yield: %d (calculated interval time %.2f ms)\n", ticksPerYield, ticksPerYield * (1/GetFramerate()) * 1000 )
+	-- local s4 = sprintf("  Yield count: %d\n", yieldCount )
+	-- local s5 = sprintf("  Calculated time suspended: %.2f ms\n", calculatedTimeSuspended )
+	local s5 = sprintf("\nThread %d\n", threadId )
+	local s6 = sprintf("  time suspended: %.2f ms\n", measuredTimeSuspended)
+	local s7 = sprintf("  Lifetime: %d ms.\n", measuredLifetime )
+	local s8 = sprintf("  Congestion: %.3f%%\n", congestion * 100 )
+
+	mf:postMsg( s5 .. s6 .. s7 .. s8 )
 	return result
 end
-
 function stats:dumpTableEntries()
+
 	local result = {SUCCESS, EMPTY_STR, EMPTY_STR}
-	local entry, numEntries = mgr:getStatsEntry()
+	local entryTable = mgr:getStatsEntries()
+	local numEntries = #entryTable
+	E:dbgPrint( sprintf("%d entries in stats table.", numEntries ))
 
-	while entry ~= nil do
-		result = stats:printEntry( entry )
-		if not result[1] then mf:postResult( result ) return end
-
-		entry, numEntries = mgr:getStatsEntry()
+	for _, e in ipairs( entryTable ) do		
+		stats:printEntry( e )
 	end
 end
 	
-
 local fileName = "ThreadStats.lua"
 if E:debuggingIsEnabled() then
 	DEFAULT_CHAT_FRAME:AddMessage( sprintf("%s loaded", fileName), 1.0, 1.0, 0.0 )
